0dcb1589f35c7740ebfa54e2c1a61d558e3308a0
1x <70488662+dot-1x@users.noreply.github.com>
2023-03-08 01:13:29 +0700

    public release

---
+++ README.md
@@ -0,0 +1,30 @@
+# How to use
+
+() = means required  
+[] = means optional  
+
+1. create a github account  
+2. create a repository and make it **private**  
+3. **DOUBLE CHECK IF ITS PRIVATE, ITS IMPORTANT!!**  
+4. set up secret env:  
+    1. Click settings  
+    2. on Security sections click secret  
+    3. add new secret by clicking `New repository secret`, then add the following env:  
+    ```
+        STARTINGNUMBER=(THE START DATA FOR CURRENT DAILY PERIOD)  inspect element income web, find `data-id` property of first item
+        SELSERVER=(THE SERVER YOU WANT TO SEND)  
+        KGPASSWORD=(YOUR KAGEHERO PASSWORD)  
+        KGUSER=(YOUR KAGEHERO EMAIL)  
+        DISCORDUSER=(YOUR DISCORD USERID)
+        TOKEN=[YOUR DISCORD BOT TOKEN FOR NOTIFICATION]
+    ```
+5. upload all files to github
+6. set up workflow folder:
+    1. create new file with the following path and name:  
+        `.github/workflows/anything.yml`
+        you don't need to write anything  
+    2. upload all `.yml` files to `.github/workflows/`  by simply clicking `add file` on  
+        upper right corner, make sure to check the path  
+7. check the running actions by clicking actions tab,  
+    look for the orange dot and click  
+    if it runs more than 3 minutes, then it is failed else success  
---
+++ dailyclaim.py
@@ -0,0 +1,164 @@
+import asyncio
+import os
+from enum import Enum, auto
+from datetime import datetime, timedelta
+from calendar import monthrange
+from typing import List, NamedTuple, TypedDict
+
+import httpx
+from discord import Bot, Embed, Colour, Forbidden
+from bs4 import BeautifulSoup
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+OWNERBOT = os.getenv("DISCORDUSER", "0")  # CHANGE THIS WITH YOUR DISCORD ACCOUNT
+USER = os.getenv("KGUSER")
+PASSWORD = os.getenv("KGPASSWORD")
+
+DATE = datetime.utcnow() + timedelta(hours=7)  # GMT + 7 datetime
+SERVER = os.getenv("SELSERVER", "1")  # IK its weird
+PERIOD = DATE.month
+STARTNUMBER = os.getenv("STARTINGNUMBER", "45")  # item starting number of current month
+TOKEN = os.getenv("TOKEN")
+_, DAYS = monthrange(DATE.year, DATE.month)
+
+bot = Bot()
+
+
+class Response(TypedDict):
+    message: str
+    data: str
+
+
+class ClaimStatus(Enum):
+    SUCCESS = auto()
+    FAILED = auto()
+    CLAIMED = auto()
+
+
+class DataTup(NamedTuple):
+    status: str
+    day: int
+    item: int
+    name: str
+
+
+MSGSMAP = {
+    ClaimStatus.SUCCESS: "**Succes** :ballot_box_with_check:",
+    ClaimStatus.FAILED: "**Failed** :x:",
+    ClaimStatus.CLAIMED: "**Claimed** :white_check_mark:",
+}
+
+
+async def perform_claim(session: httpx.AsyncClient, data: dict):
+    resp = await session.post(
+        "https://kageherostudio.com/event/index_.php?act=daily", data=data
+    )
+    resdata: Response = resp.json()
+    await asyncio.sleep(0)
+    return resdata["message"] == "success"
+
+
+async def login():
+    if USER is None or PASSWORD is None:
+        raise ValueError("Username and Password must not NONE!")
+
+    async with httpx.AsyncClient() as ses:
+        # get the cookie first
+        await ses.get("https://kageherostudio.com/event/?event=daily")
+
+        # perform login
+        await ses.post(
+            "https://kageherostudio.com/event/index_.php?act=login",
+            data={"txtuserid": USER, "txtpassword": PASSWORD},
+        )
+        resp = await ses.get("https://kageherostudio.com/event/?event=daily")
+        page = BeautifulSoup(resp.content, "html.parser")
+
+        results: List[DataTup] = [
+            DataTup(MSGSMAP[ClaimStatus.FAILED], -1, -1, '')
+        ] * DAYS  # pre allocation memory
+
+        for day, element in enumerate(page.find_all("div", "grayscale"), start=1):
+            results[day - 1] = DataTup(
+                MSGSMAP[ClaimStatus.CLAIMED],
+                day,
+                int(element["data-id"]),
+                element['data-name']
+            )
+
+        # do the reward claiming
+        for day, num in enumerate(
+            range(
+                int(STARTNUMBER),
+                int(STARTNUMBER) + DAYS,
+            ),
+            start=1,
+        ):
+            if num in results[day - 1]:
+                continue
+            result = await perform_claim(
+                ses, data={"itemId": num, "periodId": PERIOD, "selserver": int(SERVER)}
+            )
+            results[day - 1] = DataTup(
+                MSGSMAP[ClaimStatus.SUCCESS if result else ClaimStatus.FAILED],
+                day,
+                num,
+                results[day - 1].name
+            )
+            if result:  # ignoring rest as it will trigger fail to save time
+                break
+
+    await asyncio.sleep(0)
+
+    if not TOKEN:  # check without sending to discord bot
+        print(
+            *[
+                f"Item {item}/Day {day}: {f'Claimed ({name})' if item else f'Failed ({name})'}"
+                for _, day, item, name in results
+                if item
+            ],
+            sep="\n",
+        )
+    return results
+
+
+@bot.add_listener
+async def on_ready():
+    try:
+        results = await login()
+    except Exception as e: # catch anything
+        bot.loop.stop()
+        return
+
+    embed = Embed(
+        colour=Colour.nitro_pink(),
+        title="Claim daily report!",
+        description=f"**{DATE.strftime('%d/%m/%Y')}**\n"
+        + "\n".join(
+            f"Item {item}/Day {day} ({name}): " + res for res, day, item, name in results if item
+        ),
+        timestamp=datetime.now(),
+    )
+    user = await bot.get_or_fetch_user(int(OWNERBOT))
+    if not user:
+        print("User not found!")
+    else:
+        try:
+            await user.send(embed=embed)
+        except Forbidden:
+            bot.loop.stop()
+            return
+    bot.loop.stop()
+
+
+if __name__ == "__main__":
+    if TOKEN:
+        bot.run(TOKEN)
+    else:
+        asyncio.run(login())
